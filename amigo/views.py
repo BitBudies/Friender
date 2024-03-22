f47f1ca4f72e4a6d930155226575c281f8caa724
Simon <simonabastomart@gmail.com>
2024-03-22 17:51:13 -0400

    Agregando un endPoint de prueba

--- amigo/views.py
+++ amigo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+class endpoint(APIView):
+    def get(self, request):
+        return Response({'mesage': 'Al kevin le gustan los endPoints'})
+    
 # Create your views here.
