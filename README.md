bda30a8b821a03bd53ec09ee4a594b7482a8e457
Simon <simonabastomart@gmail.com>
2024-03-22 13:25:14 -0400

    agreagndo las librerias y el control de credenciales privadas, nueva modificacion en readmi

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Crea un entorno virtual. Puedes hacer esto con el comando python -m venv venv. E
 Activa el entorno virtual. En Windows, puedes hacer esto con el comando venv\Scripts\activate.
 
 Una vez que tu entorno virtual está activo, puedes instalar las dependencias necesarias con el comando pip install -r requirements.txt.
+
+una ves creado añadir las credenciales de la base de datos. Crear un archivo .env y añadir las credenciales de esta manera 
+
+DB_NAME=your_database_name
+DB_USER=your_database_user
+DB_PASSWORD=your_database_password
+DB_HOST=your_database_host
+DB_PORT=your_database_port
+
--- server/settings.py
+++ server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
