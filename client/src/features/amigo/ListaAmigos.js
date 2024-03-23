efdc04ac19d39328c60c2dc0bfed9fdae14f308a
Kevin Huayllas <KevinOmega01@gmail.com>
2024-03-23 15:14:26 -0400

    resolver problemas en lista de amigos

--- client/src/features/amigo/ListaAmigos.js
+++ client/src/features/amigo/ListaAmigos.js
@@ -14,33 +14,33 @@ const ListaAmigos = () => {
       },[amigos])
 
   return (
-    <div id='lista_amigos' className='container'>
+    <div id='lista_amigos' className='page'>
+        <div className='container'>
       <div className='grid-container'>
-                    {amigos.map((amigo, index) => (
-                        <div key={index} className='card'>
-                            <div className='card_content'>
-                               
-                              <div className='card_header' style={{backgroundImage : `url(${amigo.imagen})`}}></div>
-                                {/* Información del amigo */}
-                                <div className='card_info'>
-                                    {/* Nombre del amigo */}
-                                    <h4>{amigo.nombre}</h4>
-                                    
-                                    {/* Estrellas y número de personas atendidas */}
-                                    <div className="card_stats">
-                                        <div>★★★☆☆</div> {/* Ejemplo de estrellas */}
-                                        <div>{amigo.personasAtendidas} personas</div>
-                                    </div>
-                                    {/* Precio y botón de Ver Perfil */}
-                                    <div className="card_actions">
-                                    <Link to={`/amigos/${amigo.id}`}className='btn btn-azul'>Ver Perfil</Link>
-                                        <div>40 BOB</div>
-                                    </div>
-                                </div>
-                            </div>
+      {amigos.map((amigo, index) => (
+            <div key={index} className='card'>
+                <div className='card_header' style={{backgroundImage : `url(${amigo.imagen})`}}></div>
+                <div className='card_content'>                               
+                     {/* Información del amigo */}
+                    <div className='card_info'>
+                        {/* Nombre del amigo */}
+                        <h4>{amigo.nombre}</h4>                        
+                        {/* Estrellas y número de personas atendidas */}
+                        <div className="card_stats">
+                            <div>★★★☆☆</div> {/* Ejemplo de estrellas */}
+                                <div>{amigo.n_clientes} personas</div>
                         </div>
-                    ))}
+                        {/* Precio y botón de Ver Perfil */}
+                        <div className="card_actions">
+                            <Link to={`/amigos/${amigo.id}`}className='btn btn-azul'>Ver Perfil</Link>
+                            <div>{amigo.precio} BOB</div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+        ))}                    
+        </div>
+        </div>
     </div> 
   )
 }
--- client/src/hooks/mockFriend.js
+++ client/src/hooks/mockFriend.js
@@ -20,16 +20,17 @@ const generateMockFriends = (n) => {
             friends.push({
                 ...mockFriend,
                 id : index,puntuacion: (Math.random() * 5).toFixed(2),
-                n_clientes : Math.round(Math.random() * 40)})
+                n_clientes : Math.round(Math.random() * 40),
+                precio : Math.round(Math.random() * (40 - 5)) + 5
+            })
         }
     }  
 }
 
 const getMockFriends = () => {
     return friends.map((friend) => {
-        const {id, nombre, apellido_paterno, apellido_materno,imagen,edad,puntuacion} = friend;
-        console.log(id);
-        return {id,nombre,apellido_paterno,apellido_materno,imagen,edad,puntuacion}
+        const {descripcion, ...simpleFriend} = friend;
+       return simpleFriend;
     });
 }
 
