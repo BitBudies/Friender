20c506c3c99e3a73d8d425161ce8aac1a02cf821
Kevin Huayllas <KevinOmega01@gmail.com>
2024-03-23 19:07:44 -0400

    agregar nueva foto para la home page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>
-      <h1>Friender home page</h1>
-      <p>En desarrollo</p>
+    <div className='page '>
+      <div id='Home_section' className='bg-gradient'>
+        <h1>Friender home page</h1>
+        <p>En desarrollo</p>
+      </div>
     </div>
   )
 }
