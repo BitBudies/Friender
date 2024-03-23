9a98d1f10046d267ef3d86e9b0a4c299857598da
Kevin Huayllas <KevinOmega01@gmail.com>
2024-03-23 12:55:27 -0400

    agregar estilo a la lista de amigos

--- client/src/features/amigo/ListaAmigos.js
+++ client/src/features/amigo/ListaAmigos.js
@@ -11,15 +11,32 @@ const ListaAmigos = () => {
 
   return (
     <div id='lista_amigos' className='container'>
-      {amigos.map((amigo) => {
-        return(
-            <div key={amigo.id} className='card'>
-                <div className='card_header' style={{backgroundImage : `url(${amigo.imagen})`}}></div>
-                <h4>{amigo.nombre}</h4>
-                <Link to={`/amigos/${amigo.id}`}className='btn btn-azul'>Ver Perfil</Link>
-            </div>
-        );
-      })}
+      <div className='grid-container'>
+                    {amigos.map((amigo, index) => (
+                        <div key={index} className='card'>
+                            <div className='card_content'>
+                               
+                              <div className='card_header' style={{backgroundImage : `url(${amigo.imagen})`}}></div>
+                                {/* Información del amigo */}
+                                <div className='card_info'>
+                                    {/* Nombre del amigo */}
+                                    <h4>{amigo.nombre}</h4>
+                                    
+                                    {/* Estrellas y número de personas atendidas */}
+                                    <div className="card_stats">
+                                        <div>★★★☆☆</div> {/* Ejemplo de estrellas */}
+                                        <div>{amigo.personasAtendidas} personas</div>
+                                    </div>
+                                    {/* Precio y botón de Ver Perfil */}
+                                    <div className="card_actions">
+                                    <Link to={`/amigos/${amigo.id}`}className='btn btn-azul'>Ver Perfil</Link>
+                                        <div>40 BOB</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
     </div> 
   )
 }
