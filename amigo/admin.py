aea6a749f3b3466e32cfcc103d0afb35f774bc04
Simon <simonabastomart@gmail.com>
2024-03-23 19:26:07 -0400

    agregar los modelos al administardor

--- amigo/admin.py
+++ amigo/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models.clienteDB import Cliente
 from .models.amigoDB import Amigo
+from .models.solicitud_alquilerDB import solicitud_alquiler
+from .models.calificacionDB import Calificacion
+from .models.fotografiaDB import Fotografia
+
 
 # Register your models here.
 admin.site.register(Cliente)
 admin.site.register(Amigo)
+admin.site.register(solicitud_alquiler)
+admin.site.register(Calificacion)
+admin.site.register(Fotografia)
+
