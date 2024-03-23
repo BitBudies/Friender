afe05ab76b29c5a8eb299e1e2e6c1d8bbabe43e9
Jh0nZ <jhondeycraft776@gmail.com>
2024-03-23 18:08:46 -0400

    agregando endpoint encontrar amigo por id

--- amigo/views.py
+++ amigo/views.py
@@ -2,8 +2,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Cliente
-from .serializers.clienteserializer import ClienteSerializer
+from .models import Cliente, Amigo
+from .serializers.cliente_serializer import ClienteSerializer
+from .serializers.amigo_serializer import AmigoSerializer
+
 
 #class endpoint(APIView):
 #    def get(self, request):
@@ -17,7 +19,7 @@ class ClienteDetailById(APIView):
             return Response({"error": "Cliente no encontrado"}, status=status.HTTP_404_NOT_FOUND)
         serializer = ClienteSerializer(cliente)
         return Response(serializer.data)
-    
+
 class ClienteListView(APIView):
     def get(self, request, limite):
         try:
@@ -31,4 +33,14 @@ class ClienteListView(APIView):
             )
         clientes = Cliente.objects.all()[:limite]  # Obtener los clientes con el limite
         serializer = ClienteSerializer(clientes, many=True)
+        return Response(serializer.data)
+
+class AmigoListAPIView(APIView):
+    def get(self, request, amigo_id):
+        try:
+            amigo = Amigo.objects.get(amigo_id=amigo_id)
+        except Amigo.DoesNotExist:
+            return Response({"error": "Amigo no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = AmigoSerializer(amigo)
         return Response(serializer.data)
