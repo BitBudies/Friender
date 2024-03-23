abb2a8739ac89c09ac9b86ae2bd9fbbe0ece1080
Kevin Huayllas <KevinOmega01@gmail.com>
2024-03-23 19:30:26 -0400

    hacer responsive los items de lista amigos

--- client/src/features/amigo/ListaAmigos.js
+++ client/src/features/amigo/ListaAmigos.js
@@ -16,24 +16,26 @@ const ListaAmigos = () => {
   return (
     <div id='lista_amigos' className='page'>
         <div className='container'>
-      <div className='grid-container'>
+      <div className='row row-cols-1 row-cols-lg-5  row-cols-md-3 g-4'>
       {amigos.map((amigo, index) => (
-            <div key={index} className='card'>
+            <div key={index} className=' col'>
+                <div className='card'>
                 <div className='card_header' style={{backgroundImage : `url(${amigo.imagen})`}}></div>
                 <div className='card_content'>                               
                      {/* Información del amigo */}
-                    <div className='card_info'>
-                        {/* Nombre del amigo */}
-                        <h4>{amigo.nombre}</h4>                        
-                        {/* Estrellas y número de personas atendidas */}
-                        <div className="card_stats">
-                            <div>★★★☆☆</div> {/* Ejemplo de estrellas */}
-                                <div>{amigo.n_clientes} personas</div>
-                        </div>
-                        {/* Precio y botón de Ver Perfil */}
-                        <div className="card_actions">
-                            <Link to={`/amigos/${amigo.id}`}className='btn btn-azul'>Ver Perfil</Link>
-                            <div>{amigo.precio} BOB</div>
+                        <div className='card_info'>
+                            {/* Nombre del amigo */}
+                            <h4>{amigo.nombre}</h4>                        
+                            {/* Estrellas y número de personas atendidas */}
+                            <div className="card_stats">
+                                <div>★★★☆☆</div> {/* Ejemplo de estrellas */}
+                                    <div>{amigo.n_clientes} personas</div>
+                            </div>
+                            {/* Precio y botón de Ver Perfil */}
+                            <div className="card_actions">
+                                <Link to={`/amigos/${amigo.id}`}className='btn btn-azul'>Ver Perfil</Link>
+                                <div>{amigo.precio} BOB</div>
+                            </div>
                         </div>
                     </div>
                 </div>
