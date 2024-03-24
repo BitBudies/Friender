8f0378429d0cef3c11771c4f42528f9f120fe138
Kevin Huayllas <KevinOmega01@gmail.com>
2024-03-24 10:04:51 -0400

    arreglando conflictos de url

--- amigo/admin.py
+++ amigo/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models.clienteDB import Cliente
 from .models.amigoDB import Amigo
-<<<<<<< HEAD
 from .models.solicitud_alquilerDB import solicitud_alquiler
 from .models.calificacionDB import Calificacion
 from .models.fotografiaDB import Fotografia
@@ -14,9 +13,6 @@ admin.site.register(solicitud_alquiler)
 admin.site.register(Calificacion)
 admin.site.register(Fotografia)
 
-=======
-
 # Register your models here.
 admin.site.register(Cliente)
 admin.site.register(Amigo)
->>>>>>> 5575d703f7c7e3eef9402f5e8162a8a9467192f7
