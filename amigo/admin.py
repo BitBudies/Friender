c9a0418c835a5c518033042ee3799a335720b602
Jh0nZ <jhondeycraft776@gmail.com>
2024-03-24 10:45:54 -0400

    corregir admin

--- amigo/admin.py
+++ amigo/admin.py
@@ -12,7 +12,3 @@ admin.site.register(Amigo)
 admin.site.register(solicitud_alquiler)
 admin.site.register(Calificacion)
 admin.site.register(Fotografia)
-
-# Register your models here.
-admin.site.register(Cliente)
-admin.site.register(Amigo)
