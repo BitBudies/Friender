f9e35634ae18b54ad470f9f077d530143c402b2c
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2024-03-23 18:48:10 -0700

    Agregar scroll y Bootstrap

--- amigo/admin.py
+++ amigo/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models.clienteDB import Cliente
 from .models.amigoDB import Amigo
+<<<<<<< HEAD
 from .models.solicitud_alquilerDB import solicitud_alquiler
 from .models.calificacionDB import Calificacion
 from .models.fotografiaDB import Fotografia
@@ -12,3 +13,9 @@ admin.site.register(Amigo)
 admin.site.register(solicitud_alquiler)
 admin.site.register(Calificacion)
 admin.site.register(Fotografia)
+=======
+
+# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Amigo)
+>>>>>>> 5575d70 (Agregar scroll y Bootstrap)
