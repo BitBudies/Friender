76d7ef0f7f8c5a3be50e8e032fb1e12dac6ba54d
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2024-03-24 16:05:58 -0700

    perfil de amigos agregar info

--- client/src/features/amigo/PerfilAmigo.js
+++ client/src/features/amigo/PerfilAmigo.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { getMockFriendById } from '../../hooks/mockFriend';
-import './PerfilAmigo.css'; // Importa tu archivo CSS aquí
 
 const PerfilAmigo = () => {
   const { id_amigo } = useParams();
   const amigo = getMockFriendById(id_amigo);
 
   return (
-    <div className='perfil-amigo-container'>
-      <div className='perfil-amigo-left'>
-        <img src={amigo.foto} alt={amigo.nombre} />
-        <h3>{amigo.nombre}</h3>
-        {/* Agrega las estrellas aquí */}
-        <p>Edad: {amigo.edad} años</p>
-        {/* Agrega los rectángulos de 3 columnas y 2 filas aquí */}
-      </div>
-      <div className='perfil-amigo-right'>
-        <h2>Perfil de Amigo</h2>
-        <p>Intereses: {amigo.intereses}</p>
-        <p>Gustos: {amigo.gustos}</p>
-        <p>Descripción: {amigo.descripcion}</p>
-        <div className='cuadro-vacio'></div>
-        <p>Precio: {amigo.precio}</p>
-        <button className='solicitar-contacto-btn'>Solicitar Contacto</button>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6'>
+          <div className='perfil-amigo-left text-center'>
+            <img src={amigo.foto} alt={amigo.nombre} className='img-fluid mb-3' />
+            <h3>{amigo.nombre}</h3>
+            <div>★★★☆☆</div> 
+            <p>Edad: {amigo.edad} años</p>
+            {/* Agrega los rectángulos de 3 columnas y 2 filas aquí */}
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <div className='perfil-amigo-right p-4'>
+            <h2>Perfil de Amigo</h2>
+            <p>Intereses: {amigo.intereses}</p>
+            <p>Gustos: {amigo.gustos}</p>
+            <p>Descripción: {amigo.descripcion}</p>
+            <div className='cuadro-vacio'></div>
+            <p>Precio: {amigo.precio}</p>
+            <button className='btn btn-primary mt-3'>Solicitar Contacto</button>
+          </div>
+        </div>
       </div>
     </div>
   );
