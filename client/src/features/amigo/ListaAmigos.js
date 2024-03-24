56c7694d1441f1525284e4da9ea40c390d0959f8
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2024-03-23 17:53:42 -0700

    Agregar scroll y booBootstrap

--- client/src/features/amigo/ListaAmigos.js
+++ client/src/features/amigo/ListaAmigos.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect } from 'react';
 import { getMockFriends } from '../../hooks/mockFriend';
 import { Link } from 'react-router-dom';
@@ -47,16 +48,47 @@ const ListaAmigos = () => {
                                             </div>
                                         </div>
                                     </div>
+=======
+import React from 'react';
+import { getMockFriends } from '../../hooks/mockFriend';
+import { Link } from 'react-router-dom';
+import './listaAmigos.css';
+
+const ListaAmigos = () => {
+    // Función para obtener amigos
+    const amigos = getMockFriends();
+
+    return (
+        <div className='lista-amigos-container'>
+            <div className='container'>
+                <div className='row row-cols-1 row-cols-md-3 g-4'>
+                    {amigos.map((amigo, index) => (
+                        <div key={index} className='col'>
+                            <div className='card'>
+                                <img src={amigo.imagen} className='card-img-top' alt={amigo.nombre} />
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{amigo.nombre}</h5>
+                                    <p className='card-text'>{amigo.personasAtendidas} personas</p>
+                                    <Link to={`/amigos/${amigo.id}`} className='btn btn-primary'>
+                                        Ver Perfil
+                                    </Link>
+>>>>>>> af07beb (Agregar scroll y booBootstrap)
                                 </div>
                             </div>
                         )})}
                     </div>
                 </div>
             </div>
+<<<<<<< HEAD
         );
     }
 
     
 }
+=======
+        </div>
+    );
+};
+>>>>>>> af07beb (Agregar scroll y booBootstrap)
 
 export default ListaAmigos;
