ec0f3898e4504efe8ea9c4adea83626cfc06d6b4
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2024-03-24 17:26:23 -0700

    agrgar imagen a perfil de amigos

--- client/src/features/amigo/PerfilAmigo.js
+++ client/src/features/amigo/PerfilAmigo.js
@@ -11,21 +11,21 @@ const PerfilAmigo = () => {
       <div className='row'>
         <div className='col-md-6'>
           <div className='perfil-amigo-left text-center'>
-            <img src={amigo.foto} alt={amigo.nombre} className='img-fluid mb-3' />
+            <img src={amigo.imagen} alt={amigo.nombre} className='img-fluid mb-3' style={{ width: '300px', height: '300px' }} /> 
             <h3>{amigo.nombre}</h3>
             <div>★★★☆☆</div> 
-            <p>Edad: {amigo.edad} años</p>
+            <p><strong>Edad:</strong> {amigo.edad} años</p>
             {/* Agrega los rectángulos de 3 columnas y 2 filas aquí */}
           </div>
         </div>
         <div className='col-md-6'>
           <div className='perfil-amigo-right p-4'>
             <h2>Perfil de Amigo</h2>
-            <p>Intereses: {amigo.intereses}</p>
-            <p>Gustos: {amigo.gustos}</p>
-            <p>Descripción: {amigo.descripcion}</p>
+            <p><strong>Intereses: </strong>{amigo.intereses}</p>
+            <p><strong>Gustos:</strong> {amigo.gustos}</p>
+            <p><pre><strong>Descripción:</strong> </pre>{amigo.descripcion}</p>
             <div className='cuadro-vacio'></div>
-            <p>Precio: {amigo.precio}</p>
+            <p><h2><strong>Precio:</strong> {amigo.precio}</h2></p>
             <button className='btn btn-primary mt-3'>Solicitar Contacto</button>
           </div>
         </div>
