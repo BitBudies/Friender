ab087a21b7848d5809bb764061d47f19e15559d9
Kevin Huayllas <KevinOmega01@gmail.com>
2024-03-27 21:35:35 -0400

    Agregar algoritmo de renderizado multiple en la seccion de perfil de usuario

--- client/src/features/cliente/Perfil.js
+++ client/src/features/cliente/Perfil.js
@@ -1,10 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Perfil.css"
 import { useGlobalContext } from '../../context'
 
+const optionsData = [
+  {
+    id : 1, 
+    name : "Editar Perfil", 
+    toRender : <div className='editar-perfil'><h1>Editar Perfil</h1></div>},
+   {
+    id : 2,
+    name: 'Ver solicitudes Pendientes',
+    toRender: <></>
+  },
+  {
+    id:3,
+    name: 'Configuracion',
+    toRender: <h1>Configuracion</h1>
+  }
+]
+
+
 const Perfil = () => {
+    const [currentOption,setCurrentOption] = useState([<h1>Hola</h1>]);
 
     const {userData} = useGlobalContext();
+    const {nombre_completo} = userData
 
     useEffect(() => {
       console.log(userData)
@@ -15,15 +35,29 @@ const Perfil = () => {
           <div className='profile-section'>
               <div className='profile-options'>
                   <div className='profile-info'>
-                      <div className='profile-image'>
+                    <div className='profile-image'>
                       <div 
                       className='image' 
                       style={{backgroundImage:"url(/images/user.jpeg)"}}></div>
-                      </div>
-                      <h4>Kevin Huayllas Pinto</h4>
+                    </div>
+                    <h4>{nombre_completo}</h4>
+                  </div>
+                  <div className='options'>
+                    <ul>
+                      {optionsData.map(item => (
+                        <li key={item.id} onClick={() => setCurrentOption([item.toRender])}>
+                          <p>{item.name}</p>
+                        </li>
+                      ))}
+                      <li>
+                        <p>Cerrar Sesión</p>
+                      </li>
+                    </ul>
                   </div>
               </div>
-              <div className='profile-content'></div>
+              <div className='profile-content'>
+                {currentOption[0]}
+              </div>
           </div>
       </div>
       
