cc0fc9fad4559859d30c15a3d41e6f21b5f50930
erwinparedez <erwin.erwin.ep@gmail.com>
2024-03-30 17:29:29 -0400

    agrego funcionalidad para eliminar elementos de la bandeja con el icono de basurero para cada solicitud que se muestra en pantalla

--- client/src/features/solicitudes/SolicitudesPendientes.js
+++ client/src/features/solicitudes/SolicitudesPendientes.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import Solicitud from './Solicitud'
+import React, { useEffect } from 'react';
+import Solicitud from './Solicitud';
+import "./SolicitudesPendientes.css";
+
 
 const SolicitudesPendientes = () => {
+  useEffect(() => {
+    function ocultarElemento(event) {
+        const elementoClickeado = event.target;
+        if (elementoClickeado.classList.contains("delete-icon")) {
+            elementoClickeado.closest('.card').classList.add("no-visible");
+        }
+    }
+
+    const closeIcons = document.querySelectorAll('.delete-icon');
+    closeIcons.forEach(closeIcon => {
+        closeIcon.addEventListener('click', ocultarElemento);
+    });
+  }, []);
+  
   return (
     <div className='solicitudes-pendientes'>
       <h1>Solicitudes pendientes</h1>
-      <div className='solicitudes-pendientes-center'>
+      <div className='solicitudes-pendientes-center' id="solicitudes-box">
         {Array.from({length : 8},(_,index) => <Solicitud/>)}
       </div>
     </div>
