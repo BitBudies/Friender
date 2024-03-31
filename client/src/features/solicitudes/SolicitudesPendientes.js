10b2c4e671382ac8347494a62e841a7d4a57e3af
erwinparedez <erwin.erwin.ep@gmail.com>
2024-03-31 16:53:53 -0400

    se cambió el titulo de la seccion solicitudes recibidas

--- client/src/features/solicitudes/SolicitudesPendientes.js
+++ client/src/features/solicitudes/SolicitudesPendientes.js
@@ -20,7 +20,7 @@ const SolicitudesPendientes = () => {
   
   return (
     <div className='solicitudes-pendientes'>
-      <h1 id='titulo-solicitudes'>Solicitudes pendientes</h1>
+      <h1 id='titulo-solicitudes'>Solicitudes Recibidas</h1>
       <div className='solicitudes-pendientes-center' id="solicitudes-box">
         {Array.from({length : 8},(_,index) => <Solicitud/>)}
       </div>
