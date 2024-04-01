1c44fdf7af94e4eccc2f928c8a8646324ca1b323
Kevin Huayllas <KevinOmega01@gmail.com>
2024-04-01 17:01:42 -0400

    agregar todos los componentes necesarios para los detalles de una solicitud

--- client/src/features/solicitudes/SolicitudDetalles.js
+++ client/src/features/solicitudes/SolicitudDetalles.js
@@ -10,7 +10,7 @@ const SolicitudDetalles = () => {
     const {data:solicitud,isFetching,isSuccess} = useGetSolicitudPendienteByIdQuery(id_solicitud)
 
     useEffect(() => {
-        console.log(solicitud)
+        console.log(solicitud,"solicitud")
     },[solicitud])
 
     if(isFetching){
@@ -21,14 +21,27 @@ const SolicitudDetalles = () => {
                 <div className='solicitud-detalles-center'>
                     <div className='cliente-info'>
                         <div className='profile-image' style={{backgroundImage: "url(/images/user.jpeg)"}}></div>
-                        <h4>Kevin Huayllas Pinto</h4>
+                        <h4>{solicitud.nombre_cliente}</h4>
                         <span>★★★☆☆</span>
-                        <p>Edad: 28 años</p>
+                        <p>Edad: {solicitud.edad_cliente} años</p>
                         <Link className='btn btn-azul'>Ver Perfil</Link>
                     </div>
-                    <div className='solicitud-details'></div>
+                    <div className='solicitud-details'>
+                        <h3>Detalles de la solicitud</h3>
+                        <p><strong>Fecha: </strong> {solicitud.fecha_inicio}</p>
+                        <p><strong>Hora: </strong> {solicitud.hora_inicio}</p>
+                        <p><strong>Tiempo: </strong> {solicitud.minutos}</p>
+                        <p><strong>Lugar: </strong> {solicitud.lugar}</p>
+                        <p><strong>Descripcion:</strong></p>
+                        <p>{solicitud.descripcion}</p>
+                        <h5>Total: {10 * solicitud.minutos} $us </h5>
+                        <div className='btns'>
+                            <button className='btn btn-success'>Aceptar</button>
+                            <button className='btn btn-danger'>Aceptar</button>
+
+                        </div>
+                    </div>
                 </div>
-              {id_solicitud}
             </div>
           )   
     }
