df196fdd3ab702868c80bb05b8520dcb0086afec
Kevin Huayllas <KevinOmega01@gmail.com>
2024-04-03 00:02:41 -0400

    agregar conexion con el servidor deployado

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "/api",
+    baseUrl: "https://friender-6lbr.onrender.com/",
   }),
   tagTypes: ["Amigo","Auth","Cliente","Test","Solicitud"],
   endpoints: (builder) => ({}),
