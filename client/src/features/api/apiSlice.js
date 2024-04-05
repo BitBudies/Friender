08134a1035c5714bbf4884390294d9ba0b521173
Kevin Huayllas <KevinOmega01@gmail.com>
2024-04-05 17:25:59 -0400

    agregar conexion con el servidor de django en desarrollo

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://friender-6lbr.onrender.com/api/",
+    baseUrl: "/api/",
   }),
   tagTypes: ["Amigo","Auth","Cliente","Test","Solicitud"],
   endpoints: (builder) => ({}),
