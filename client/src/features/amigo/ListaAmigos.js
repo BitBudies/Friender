afd289f9a72c4fb06600121c368b4a9c17279f6b
Kevin Huayllas <KevinOmega01@gmail.com>
2024-04-13 12:30:06 -0400

    agregar funcion de desactivar los botones de navegacion en la parte inferior de la lista deamigos, cuando estos se encuentren al limite

--- client/src/features/amigo/ListaAmigos.js
+++ client/src/features/amigo/ListaAmigos.js
@@ -63,9 +63,10 @@ const ListaAmigos = () => {
                     </p>
                 }
                 <nav aria-label="Page navigation example">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item">
-                            <Link class="page-link" to={`/amigos/page/${Number(n_page) > 1 ? Number(n_page) - 1 : Number(n_page)}`}> {"<"} </Link>
+                    <ul className="pagination justify-content-center">
+                        <li className="page-item">
+                            <Link className={`page-link ${Number(n_page) === 1 && "disabled"}`} 
+                            to={`/amigos/page/${Number(n_page) > 1 ? Number(n_page) - 1 : Number(n_page)}`}> {"<"} </Link>
                         </li>
                         {Array.from({length: amigos.numero_paginas},(_,index) => {
                             console.log(Number(n_page) === index + 1);
@@ -73,8 +74,9 @@ const ListaAmigos = () => {
                                 <Link className='page-link' to={`/amigos/page/${index + 1}`}>{index + 1}</Link>
                             </li>
                         })}
-                        <li class="page-item">
-                        <Link class="page-link" to={`/amigos/page/${Number(n_page) < amigos.numero_paginas ? Number(n_page) + 1 : Number(n_page)}`} >
+                        <li className="page-item">
+                        <Link className={`page-link ${Number(n_page) === amigos.numero_paginas && "disabled"}`} 
+                        to={`/amigos/page/${Number(n_page) < amigos.numero_paginas ? Number(n_page) + 1 : Number(n_page)}`} >
                             {">"}
                         </Link>
                         </li>
