fb066372ed56b946b745ed6d2163cd46a9ae2ea5
erwinparedez <erwin.erwin.ep@gmail.com>
2024-04-13 14:18:49 -0400

    Los mensajes de error ahora se muestran debajo de cada campo correspondiente en el formulario para alquilar un amigo

--- client/src/features/solicitudes/SolicitudDetalles.js
+++ client/src/features/solicitudes/SolicitudDetalles.js
@@ -44,11 +44,11 @@ const SolicitudDetalles = () => {
     function formatDateTime(timestamp) {
         const date = new Date(timestamp);
         const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0'); // Add leading zero for single-digit months
-        const day = String(date.getDate()).padStart(2, '0');  // Add leading zero for single-digit days
-        const hours = String(date.getHours()).padStart(2, '0'); // Add leading zero for single-digit hours
-        const minutes = String(date.getMinutes()).padStart(2, '0'); // Add leading zero for single-digit minutes
-        const seconds = String(date.getSeconds()).padStart(2, '0'); // Add leading zero for single-digit seconds
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0'); 
+        const minutes = String(date.getMinutes()).padStart(2, '0'); 
+        const seconds = String(date.getSeconds()).padStart(2, '0');
 
         return `${day}/${month}/${year}  ${hours}:${minutes}:${seconds}`;
     }
