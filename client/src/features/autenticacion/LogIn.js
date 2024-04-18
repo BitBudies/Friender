2f03bb9ca4a15598ad5d0d22d72fef882dc0ed91
erwinparedez <erwin.erwin.ep@gmail.com>
2024-04-18 09:37:37 -0400

    Se agregó el formulario  de 'iniciar sesión'

--- client/src/features/autenticacion/LogIn.js
+++ client/src/features/autenticacion/LogIn.js
@@ -1,58 +1,76 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom';
+import "./Login.css";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 
 const LogIn = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [showFeedback, setShowFeedback] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showFeedback, setShowFeedback] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
+  const handleBtn = () => {
+    console.log("pip");
+  };
 
-    const handleBtn = () => {
-        console.log("pip");
-    }
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className="form-section mw-100 min-vh-100 d-flex flex-column justify-content-center align-items-center">
-      <div className="form-section-center p-5">
-        <h1 className="mb-4 text-start fw-bold">Iniciar Sesión</h1>
-        <form>
-          <div className="form-item mb-3">
-            <label htmlFor="username " className="form-label fw-semibold">
-              Nombre de usuario
-            </label>
+      <div className='login-logo-box'>
+        <img src="https://cdn-icons-png.flaticon.com/512/7081/7081305.png" alt="icono-friender"></img>
+        <h1>Friender</h1>
+      </div>
+
+      <div className="form-section-center login-box">
+        <p className="mb-4 fw-bold login-box-title">Inicia sesión en Friender</p>
+        <form className='login-form'>
+          <div className="mb-2">
             <input
               type="text"
               className="form-control"
               id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value.trim())}
+              placeholder='Correo electrónico'
             />
           </div>
-          <div className="form-item mb-4">
-            <label htmlFor="password" className="form-label fw-semibold">
-              Contraseña
-            </label>
+
+          <div className="mb-2 password-input">
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               className="form-control"
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              placeholder='Contraseña'
             />
+            <span className="password-icon" onClick={toggleShowPassword}>
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
           </div>
+
           {showFeedback && (
-            <p className="text-danger mb-4" style={{ fontSize: "0.9rem" }}>
-              El nombre de usuario o la contraseña son incorrectas
+            <p className="text-danger mb-2" style={{ fontSize: "0.9rem",textAlign: "left",paddingLeft: "20px" }}>
+              Contraseña incorrecta
             </p>
           )}
-          <p className='form-text'>
-            <Link to={"/register"}>Olvide mi contraseña</Link>
-          </p>
-          <button className="btn btn-azul mb-2" onClick={handleBtn}>
+          
+          <button className="btn btn-azul mb-2 button-login" onClick={handleBtn}>
             Iniciar Sesión
           </button>
-          <p className="form-text">
-            No tienes una cuenta<Link to={"/register"}> haz click aquí.</Link>
+          <p className='form-text'>
+            <Link to={"/register"}>¿Haz olvidado la contraseña?</Link>
+          </p>
+          <div className='login-box-separator'>
+            <hr></hr>
+            <p>O</p>
+            <hr></hr>
+          </div>
+          <p className="form-text form-register-text">
+            ¿No tienes una cuenta?<Link to={"/register"}> Regístrate</Link>
           </p>
         </form>
       </div>
