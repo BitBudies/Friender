c4d289442d3728188d026cb0dc1e494fb0f884d1
Frosmin <simonabastomart@gmail.com>
2024-04-19 10:17:19 -0400

    probando si envia correos

--- server/settings.py
+++ server/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'personafalsa000@gmail.com'
+EMAIL_HOST_USER = 'friender.oficial@gmail.com'
 EMAIL_HOST_PASSWORD = 'veii dszd njol moyb'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
