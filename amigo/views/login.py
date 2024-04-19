a6cc5257929443f3aebc5220fab9976aff54e25c
Daye0210 <202202138@est.umss.edu>
2024-04-18 23:49:53 -0400

    response

--- amigo/views/login.py
+++ amigo/views/login.py
@@ -5,8 +5,15 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from amigo.serializers.cliente_serializer import UserTokenSerializer
 from ..serializers.login_serializer import LoginSerializer
-
+# Para instalar
+# pip install --upgrade djangorestframework-simplejwt
 class Login(ObtainAuthToken):
     def post(self,request,*args,**kwargs):
          username = request.data.get('username')
          password = request.data.get('password')
+         login_serializer = self.serializer_class(data = request.data, context = {'request':request})
+         
+         if not all([username, password]):
+            return Response({"error": "Todos los campos son requeridos"}, status=status.HTTP_400_BAD_REQUEST)
+
+    
