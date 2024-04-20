bc267c44b0880ecafe0deacc6093efa221da63d2
Kevin Huayllas <KevinOmega01@gmail.com>
2024-04-20 11:33:49 -0400

    hacer responsive el tamano del form

--- client/src/features/autenticacion/RegistrarDatos.js
+++ client/src/features/autenticacion/RegistrarDatos.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { FaLocationDot } from "react-icons/fa6";
 
 const defaultValues = {
-  nombre: '',
-  apellido_paterno: '',
-  apellido_materno: '',
-  fecha_nacimiento: '',
-  genero: '',
-  ubicacion: '',
-  nombre_usuario: '',
-  correo_electronico: '',
-  contraseña: '',
-  confirmar_contraseña: '',
+  nombre: "",
+  apellido_paterno: "",
+  apellido_materno: "",
+  fecha_nacimiento: "",
+  genero: "",
+  ubicacion: "",
+  nombre_usuario: "",
+  correo_electronico: "",
+  contraseña: "",
+  confirmar_contraseña: "",
 };
 
-const RegistrarDatos = ({setNForm}) => {
-  const [password, setPassword] = useState('');
-  const [password1, setPassword1] = useState('');
+const RegistrarDatos = ({ setNForm }) => {
+  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
   const [values, setValues] = useState(defaultValues);
   const [showPassword, setShowPassword] = useState(false);
   const [showPassword1, setShowPassword1] = useState(false);
@@ -47,72 +47,71 @@ const RegistrarDatos = ({setNForm}) => {
     const eightyYearsAgo = new Date(currentDate);
     eightyYearsAgo.setFullYear(currentDate.getFullYear() - 80);
     let messageFecha = "";
-  
+
     if (selectedDate >= eighteenYearsAgo && selectedDate <= eightyYearsAgo) {
       messageFecha = "La edad debe ser mayor a 18 y menor a 80 años";
     } else if (selectedDate >= currentDate) {
       messageFecha = "La fecha seleccionada es posterior a la fecha actual";
     }
     return messageFecha;
-  }
+  };
 
+  const NombreValido = (value) => {
+    let messageNombre = "";
 
-const NombreValido = (value) => {
-  let messageNombre = "";
+    if (!value.trim()) {
+      messageNombre = "El nombre es obligatorio";
+    }
 
-  if (!value.trim()) {
-    messageNombre = "El nombre es obligatorio";
-  }
+    return messageNombre;
+  };
+  const Apellidopaterno = (value) => {
+    let messageNombre = "";
 
-  return messageNombre;
-}
-const Apellidopaterno = (value) => {
-  let messageNombre = "";
+    if (!value.trim()) {
+      messageNombre = "El apellido paterno es obligatorio";
+    }
 
-  if (!value.trim()) {
-    messageNombre = "El apellido paterno es obligatorio";
-  }
+    return messageNombre;
+  };
 
-  return messageNombre;
-}
+  const NombreUsuario = (value) => {
+    let messageNombre = "";
 
-const NombreUsuario = (value) => {
-  let messageNombre = "";
+    if (!value.trim()) {
+      messageNombre = "El nombre de usuario es obligatorio";
+    }
 
-  if (!value.trim()) {
-    messageNombre = "El nombre de usuario es obligatorio";
-  }
+    return messageNombre;
+  };
+  const correoValidar = (value) => {
+    let messageNombre = "";
 
-  return messageNombre;
-}
-const correoValidar = (value) => {
-  let messageNombre = "";
+    if (!value.trim()) {
+      messageNombre = "El correo electrónico es obligatorio";
+    }
 
-  if (!value.trim()) {
-    messageNombre = "El correo electrónico es obligatorio";
-  }
+    return messageNombre;
+  };
+  const contraseñaValidar = (value) => {
+    let messageNombre = "";
 
-  return messageNombre;
-}
-const contraseñaValidar = (value) => {
-  let messageNombre = "";
+    if (!value.trim()) {
+      messageNombre = "La contraseña es obligatoria";
+    }
+    return messageNombre;
+  };
+  const confirmarcontraValidar = (value) => {
+    let messageNombre = "";
 
-  if (!value.trim()) {
-    messageNombre = "La contraseña es obligatoria";
-  }
-  return messageNombre;
-}
-const confirmarcontraValidar = (value) => {
-  let messageNombre = "";
+    if (!value.trim()) {
+      messageNombre = "Confirmar Contraseña es obligatorio";
+    } else if (value !== password) {
+      messageNombre = "Las contraseñas no coinciden, intente de nuevo.";
+    }
 
-  if (!value.trim()) {
-    messageNombre = "Confirmar Contraseña es obligatorio";
-  } else if (value !== password) {
-    messageNombre = "Las contraseñas no coinciden, intente de nuevo.";
-  }
-
-  return messageNombre;
-}
+    return messageNombre;
+  };
 
   return (
     <div className="form-item">
@@ -128,13 +127,13 @@ const confirmarcontraValidar = (value) => {
             placeholder="Nombre"
             value={values.nombre}
             onChange={handleChange}
-            style={{ width: '160px' }}
+            style={{ width: "160px" }}
             className="form-control"
             required
           />
-           <p className='text-danger'>{NombreValido(values.nombre)}</p>
+          <p className="text-danger">{NombreValido(values.nombre)}</p>
         </div>
-        <div className="mb-2 input-item" style={{ marginLeft: '60px' }}>
+        <div className="mb-2 input-item" style={{ marginLeft: "60px" }}>
           <label htmlFor="apellido_paterno" className="input-label">
             Apellido Paterno:
           </label>
@@ -145,13 +144,15 @@ const confirmarcontraValidar = (value) => {
             placeholder="Apellido Paterno"
             value={values.apellido_paterno}
             onChange={handleChange}
-            style={{ width: '160px' }}
+            style={{ width: "160px" }}
             className="form-control"
             required
           />
-            <p className='text-danger'>{Apellidopaterno(values.apellido_paterno)}</p>
+          <p className="text-danger">
+            {Apellidopaterno(values.apellido_paterno)}
+          </p>
         </div>
-        <div className="mb-2 input-item" style={{ marginLeft: '20px' }}>
+        <div className="mb-2 input-item" style={{ marginLeft: "20px" }}>
           <label htmlFor="apellido_materno" className="input-label">
             Apellido Materno:
           </label>
@@ -162,7 +163,7 @@ const confirmarcontraValidar = (value) => {
             placeholder="Apellido Materno"
             value={values.apellido_materno}
             onChange={handleChange}
-            style={{ width: '160px' }}
+            style={{ width: "160px" }}
             className="form-control"
           />
         </div>
@@ -179,53 +180,60 @@ const confirmarcontraValidar = (value) => {
             placeholder="dd/mm/aa"
             value={values.fecha_nacimiento}
             onChange={handleChange}
-            
-            style={{ width: '160px' }}
+            style={{ width: "160px" }}
             className="form-control"
             required
           />
-            <p className='text-danger'>{fechaValida(values.fecha_nacimiento)}</p>
-
+          <p className="text-danger">{fechaValida(values.fecha_nacimiento)}</p>
         </div>
-        <div className="mb-2 input-item" style={{ marginLeft: '60px' }}>
+        <div className="mb-2 input-item" style={{ marginLeft: "60px" }}>
           <label htmlFor="genero" className="input-label">
             Género:
           </label>
           <select
-         
             id="genero"
             name="genero"
             value={values.genero}
             onChange={handleChange}
-            style={{ width: '160px' }}
+            style={{ width: "160px" }}
             className="form-control"
             required
           >
-            
             <option value=""></option>
             <option value="masculino">Masculino</option>
             <option value="femenino">Femenino</option>
             <option value="otro">Otro</option>
           </select>
         </div>
-        <div className="mb-2 input-item" style={{ marginLeft: '60px', position: 'relative' }}>
-  <label htmlFor="ubicacion" className="input-label">
-    Ubicación:
-  </label>
-  <div style={{ display: 'flex', alignItems: 'center' }}>
-    <input
-      type="text"
-      id="ubicacion"
-      name="ubicacion"
-      placeholder="Ubicación"
-      value={values.ubicacion}
-      onChange={handleChange}
-      style={{ width: '140px', paddingRight: '30px' }}
-      className="form-control"
-    />
-    <FaLocationDot style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-0%)', color: 'black' }} />
-  </div>
-</div>
+        <div
+          className="mb-2 input-item"
+          style={{ marginLeft: "60px", position: "relative" }}
+        >
+          <label htmlFor="ubicacion" className="input-label">
+            Ubicación:
+          </label>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <input
+              type="text"
+              id="ubicacion"
+              name="ubicacion"
+              placeholder="Ubicación"
+              value={values.ubicacion}
+              onChange={handleChange}
+              style={{ width: "140px", paddingRight: "30px" }}
+              className="form-control"
+            />
+            <FaLocationDot
+              style={{
+                position: "absolute",
+                right: "10px",
+                top: "50%",
+                transform: "translateY(-0%)",
+                color: "black",
+              }}
+            />
+          </div>
+        </div>
       </div>
       <div className="input-group registro">
         <div className="mb-2 input-item">
@@ -239,13 +247,13 @@ const confirmarcontraValidar = (value) => {
             placeholder="Usuario"
             value={values.nombre_usuario}
             onChange={handleChange}
-            style={{ width: '280px' }}
+            style={{ width: "280px" }}
             className="form-control"
             required
           />
-            <p className='text-danger'>{ NombreUsuario(values.nombre_usuario)}</p>
+          <p className="text-danger">{NombreUsuario(values.nombre_usuario)}</p>
         </div>
-        <div className="mb-2 input-item" style={{ marginLeft: '30px' }}>
+        <div className="mb-2 input-item" style={{ marginLeft: "30px" }}>
           <label htmlFor="correo_electronico" className="input-label">
             Correo Electrónico:
           </label>
@@ -256,11 +264,13 @@ const confirmarcontraValidar = (value) => {
             placeholder="ejemplo: @gmail.com"
             value={values.correo_electronico}
             onChange={handleChange}
-            style={{ width: '280px' }}
+            style={{ width: "280px" }}
             className="form-control"
             required
           />
-          <p className='text-danger'>{correoValidar(values.correo_electronico)}</p>
+          <p className="text-danger">
+            {correoValidar(values.correo_electronico)}
+          </p>
         </div>
       </div>
       <div className="input-group registro">
@@ -269,44 +279,59 @@ const confirmarcontraValidar = (value) => {
             Contraseña:
           </label>
           <input
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             className="form-control"
             id="contraseña"
             placeholder="Contraseña"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            style={{ width: '280px' }}
+            style={{ width: "280px" }}
             required
           />
-            <p className='text-danger'>{ contraseñaValidar(password)}</p>
-          <span className="password-icon" onClick={toggleShowPassword} style={{ position: 'absolute', right: '10px', top: '48%', transform: 'translateY(-30%)' }}>
-  {showPassword? <FaEyeSlash /> : <FaEye />}
-</span>
+          <p className="text-danger">{contraseñaValidar(password)}</p>
+          <span
+            className="password-icon"
+            onClick={toggleShowPassword}
+            style={{
+              position: "absolute",
+              right: "10px",
+              top: "48%",
+              transform: "translateY(-30%)",
+            }}
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </span>
         </div>
-        <div className="mb-2 " style={{ marginLeft: '30px' }}>
+        <div className="mb-2 " style={{ marginLeft: "30px" }}>
           <label htmlFor="confirmar_contraseña" className="input-label">
             Confirmar Contraseña:
           </label>
           <input
-            type={showPassword1 ? 'text' : 'password'}
+            type={showPassword1 ? "text" : "password"}
             id="confirmar_contraseña"
             className="form-control"
             placeholder="Confirmar Contraseña"
-            value={password1}input-item
+            value={password1}
+            input-item
             onChange={(e) => setPassword1(e.target.value)}
-            style={{ width: '280px' }}
+            style={{ width: "280px" }}
             required
           />
-            <p className='text-danger'>{ confirmarcontraValidar(password)}</p>
-         <span className="password-icon" onClick={toggleShowPassword1} style={{ position: 'absolute', right: '180px', top: '30%', transform: 'translateY(-30%)' }}>
-  {showPassword1 ? <FaEyeSlash /> : <FaEye />}
-</span>
-       
+          <p className="text-danger">{confirmarcontraValidar(password)}</p>
+          {/* <span className="password-icon" onClick={toggleShowPassword1}>
+            {showPassword1 ? <FaEyeSlash /> : <FaEye />}
+          </span> */}
+        </div>
+        <div className="para-boton ">
+          <button
+            className="btn btn-azul input-item "
+            onClick={() => setNForm((n) => n + 1)}
+          >
+            Siguiente
+          </button>
+        </div>
       </div>
-      <div className='para-boton '>
-      <button className="btn btn-azul input-item " onClick={() => setNForm(n => n + 1)}>Siguiente</button>
-      
-    </div></div></div>
+    </div>
   );
 };
 
--- client/src/features/autenticacion/Registrarse.js
+++ client/src/features/autenticacion/Registrarse.js
@@ -1,50 +1,60 @@
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Registrarse.css";
-import { Link, useNavigate } from 'react-router-dom';
-import RegistrarDatos from './RegistrarDatos';
-import RegistrarDatos2 from './RegistrarDatos2';
-
-
+import { Link, useNavigate } from "react-router-dom";
+import RegistrarDatos from "./RegistrarDatos";
+import RegistrarDatos2 from "./RegistrarDatos2";
 
 const Registrarse = () => {
-  
-  const [nForm,setNForm] = useState(0);
-
+  const [nForm, setNForm] = useState(0);
 
   const props = {
     setNForm,
-    nForm
-  }
+    nForm,
+  };
 
   return (
-      <div className="page regist">
-      <div className="registrarse-left">
-          <h1 className='mb-3'><b>Friender</b></h1>
-          
-          <h5><p>¡Enciende la diversión con Friender!</p></h5>
-          <h5><p>Alquila amigos y crea momentos</p></h5>
-          <h5><p>inolvidables.</p></h5>
-          <img className="logo-img" src="https://cdn-icons-png.flaticon.com/512/7081/7081305.png" alt="icono-friender"></img>
+    <div className="page">
+      <div className="regist">
+        <div className="registrarse-left">
+          <h1 className="mb-3">
+            <b>Friender</b>
+          </h1>
+
+          <h5>
+            <p>¡Enciende la diversión con Friender!</p>
+          </h5>
+          <h5>
+            <p>Alquila amigos y crea momentos</p>
+          </h5>
+          <h5>
+            <p>inolvidables.</p>
+          </h5>
+          <img
+            className="logo-img"
+            src="https://cdn-icons-png.flaticon.com/512/7081/7081305.png"
+            alt="icono-friender"
+          ></img>
+        </div>
+        <div className="registrarse-right">
+          <div className="form-indicators">
+            <div className={`indicator ${nForm === 0 && "active"}`}></div>
+            <div className={`indicator ${nForm === 1 && "active"}`}></div>
+          </div>
+          <h1 className="mb-2">Regístrate</h1>
+          <div className="toColumns"></div>
+          <div className="form-registro">
+            <div
+              className="form-carousel"
+              style={{ transform: `translate(${nForm * -50}%)` }}
+            >
+              <RegistrarDatos {...props} />
+              <RegistrarDatos2 {...props} />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='registrarse-right'>
-        <div className="form-indicators">
-          <div className={`indicator ${nForm === 0 && "active"}`}></div>
-          <div className={`indicator ${nForm === 1 && "active"}`}></div>
-        </div>
-        <h1 className='mb-2'>Regístrate</h1>
-        <div className="toColumns"></div>
-        <div className='form-registro'>
-          <div className="form-carousel" style={{transform: `translate(${nForm * -50}%)`}}>
-            <RegistrarDatos {...props}/>
-            <RegistrarDatos2 {...props}/>
-          </div>  
-        </div>
-        
-        </div>
-      </div>
-    
+    </div>
   );
-}
+};
 
 export default Registrarse;
