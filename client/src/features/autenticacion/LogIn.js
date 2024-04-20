ca6eef1ea385a720055d8bf06311fe4f398b0753
erwinparedez <erwin.erwin.ep@gmail.com>
2024-04-19 22:24:58 -0400

    Se restauró el enlace para recuperar contraseña olvidada y se mejoró el diseño responsive del formulario de inicio de sesión

--- client/src/features/autenticacion/LogIn.js
+++ client/src/features/autenticacion/LogIn.js
@@ -52,12 +52,9 @@ const LogIn = () => {
   return (
     
     <div className="form-section mw-100 min-vh-100 d-flex flex-column justify-content-rigth align-items-center">
-      <h1>Friender</h1>
-      <br/>
       <div className='login-logo-box'>
-      
         <img src="https://cdn-icons-png.flaticon.com/512/7081/7081305.png" alt="icono-friender"></img>
-       
+        <h1>Friender</h1>
       </div>
 
       <div className="form-section-rigth login-box">
@@ -97,11 +94,11 @@ const LogIn = () => {
           <button className={`btn btn-azul mb-2 button-login ${disableBtn && "disabled"}`} onClick={handleBtn}>
             Iniciar Sesión
           </button>
-          {/*
+          
           <p className='form-text'>
-            <Link to={"/register"}>¿Haz olvidado la contraseña?</Link>
+            <Link to={"/resetPassword"}>¿Haz olvidado la contraseña?</Link>
           </p> 
-          */}
+          
           <div className='login-box-separator'>
             <hr></hr>
             <p>O</p>
