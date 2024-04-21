bb4d4abbd41b6fc0a6e78899410c4f53dade2633
Jh0nZ <jhondeycraft776@gmail.com>
2024-04-21 14:04:30 -0400

    corregir login front

--- client/src/features/autenticacion/authSlice.js
+++ client/src/features/autenticacion/authSlice.js
@@ -7,6 +7,9 @@ const authApi = apiSlice.injectEndpoints({
         url : "/cliente/login",
         method : "POST",
         body : data,
+        headers: {
+          "X-CSRFToken": getCookie("csrftoken") // Obtener el token CSRF desde las cookies
+        },
       })
     }),
     getIntereses : builder.query({
