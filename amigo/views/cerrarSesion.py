936abe20becc38941611b97c2fdbd5bda291cc1b
Daye0210 <202202138@est.umss.edu>
2024-04-21 23:21:30 -0400

    url

--- amigo/views/cerrarSesion.py
+++ amigo/views/cerrarSesion.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
+
 class Logout(APIView):
     permission_classes = [IsAuthenticated]
 
