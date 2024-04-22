f0ce5fc42c75e0521c24632323433b194cbedcb6
Frosmin <simonabastomart@gmail.com>
2024-04-22 00:33:54 -0400

    Agregando metodo que verifica correo

--- amigo/views/utils.py
+++ amigo/views/utils.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from decouple import config
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
+import re
 
 
 def calcular_edad(fecha_nacimiento):
@@ -26,3 +27,10 @@ def enviar_correo_prueba(request):
 def obtener_csrf(request):
     csrf_token = get_token(request)
     return JsonResponse({'csrf_token': csrf_token})
+
+def correo_valido(correo):
+    regex_correo = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if(re.search(regex_correo,correo)):
+            return True
+    else:
+            return False
