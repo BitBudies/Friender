d4e0debf1875e9ba6528062b991ecb49a15137cf
Jh0nZ <jhondeycraft776@gmail.com>
2024-04-23 19:04:41 -0400

    agregar mejoras al perfil de un amigo, y el perfil del cliente

--- client/src/features/amigo/PerfilAmigo.js
+++ client/src/features/amigo/PerfilAmigo.js
@@ -1,19 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useGetAmigoByIdQuery } from './amigoSlice';
-import Loading from '../../Components/Loading';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useGetAmigoByIdQuery } from "./amigoSlice";
+import Loading from "../../Components/Loading";
 import "./PerfilAmigo.css";
-import Formulario from '../solicitudes/Formulario';
-import { useCookies } from 'react-cookie';
+import Formulario from "../solicitudes/Formulario";
+import { useCookies } from "react-cookie";
 
 const PerfilAmigo = () => {
-  const [cookies] = useCookies(['token']);
+  const [cookies] = useCookies(["token"]);
   const token = cookies.token;
 
   const { id_amigo } = useParams();
-  const { data: amigo, isFetching, isSuccess } = useGetAmigoByIdQuery({id_amigo, token});
-  const [showForm,setShowForm] = useState(false)
-  const [formStatus,setFormStatus] = useState({sent : false, message: '',show: false})
+  const {
+    data: amigo,
+    isFetching,
+    isSuccess,
+  } = useGetAmigoByIdQuery({ id_amigo, token });
+  const [showForm, setShowForm] = useState(false);
+  const [formStatus, setFormStatus] = useState({
+    sent: false,
+    message: "",
+    show: false,
+  });
+  const calificacionEstrellas = (calificacion) => {
+    const numEstrellas = Math.round(calificacion);
+    const estrellas = '★'.repeat(numEstrellas) + '☆'.repeat(5 - numEstrellas);
+    return estrellas;
+  };
 
   useEffect(() => {
     console.log(amigo, isFetching, isSuccess);
@@ -23,46 +36,72 @@ const PerfilAmigo = () => {
     return <Loading />;
   } else if (isSuccess) {
     return (
-      <div className='page'>
-          <div className='perfil-amigo-container'>
-              <div className=' perfil-amigo-left '>
-                <div className='image-container' 
-                  style={{ backgroundImage: `url(${amigo.imagenBase64 ? 'data:image/jpeg;base64,' + amigo.imagenBase64 : '/images/user.jpeg'})` }}
-                />
-                <div className='stars-and-name' style={{ fontSize: '35px' }}>
-                      <div className='text-warning'>★★★☆☆</div>
-                      <h3>{amigo.nombre_completo}</h3>
-                </div>
-                <p><strong>Edad:</strong>18 años</p>
-      
+      <div className="page">
+        <div className="perfil-amigo-container">
+          <div className=" perfil-amigo-left ">
+            <div
+              className="image-container"
+              style={{
+                backgroundImage: `url(${
+                  amigo.imagenBase64
+                    ? "data:image/jpeg;base64," + amigo.imagenBase64
+                    : "/images/user.jpeg"
+                })`,
+              }}
+            />
+            <div className="stars-and-name" style={{ fontSize: "35px" }}>
+              <div className="text-warning">
+                {calificacionEstrellas(amigo.calificacion)}
               </div>
-              <div className='perfil-amigo-right'>
-                  <h1>Perfil de amigo</h1>
-                  <div className='profile-description w-100'>
-                    <p><strong>Descripción:</strong> {amigo.descripcion}</p>
-                  </div>
-                  
-                  <p><h4><strong>Precio:</strong> {amigo.precio_amigo} Bs/hr</h4></p>
-                  {formStatus.sent ? 
-                    <div class={`profile-alert ${!formStatus.show && "hide"} alert alert-success`} role="alert">
-                      <strong>{formStatus.message}</strong> 
-                      
-                    </div> :
-                    <div className='btn-container'>
-                      <button className='btn btn-azul mt-3 btn-solicitar' onClick={() => setShowForm(true)}>Solicitar Cita</button>
-                    </div>
-                  }
-              </div>
-
+              <h3>{amigo.nombre_completo}</h3>
+            </div>
+            <p>
+              <strong>Edad:</strong> {amigo.edad} años
+            </p>
           </div>
-        <Formulario amigo_id = {id_amigo}
-                    precio={amigo.precio_amigo} 
-                    showForm={showForm} 
-                    setShowForm={setShowForm} 
-                    formStatus={formStatus}
-                    setFormStatus ={setFormStatus} />
+          <div className="perfil-amigo-right">
+            <h1>Perfil de amigo</h1>
+            <div className="profile-description w-100">
+              <p>
+                <strong>Descripción:</strong> {amigo.descripcion}
+              </p>
+            </div>
+
+            <p>
+              <h4>
+                <strong>Precio:</strong> {amigo.precio_amigo} Bs/hr
+              </h4>
+            </p>
+            {formStatus.sent ? (
+              <div
+                class={`profile-alert ${
+                  !formStatus.show && "hide"
+                } alert alert-success`}
+                role="alert"
+              >
+                <strong>{formStatus.message}</strong>
+              </div>
+            ) : (
+              <div className="btn-container">
+                <button
+                  className="btn btn-azul mt-3 btn-solicitar"
+                  onClick={() => setShowForm(true)}
+                >
+                  Solicitar Cita
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+        <Formulario
+          amigo_id={id_amigo}
+          precio={amigo.precio_amigo}
+          showForm={showForm}
+          setShowForm={setShowForm}
+          formStatus={formStatus}
+          setFormStatus={setFormStatus}
+        />
       </div>
-      
     );
   }
 };
