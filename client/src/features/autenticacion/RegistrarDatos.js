698deaf05fc7f11880ac757adcb68c7f5e5f728a
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2024-04-27 16:27:00 -0700

    Agreagndo cursor pointer en contrasenia

--- client/src/features/autenticacion/RegistrarDatos.js
+++ client/src/features/autenticacion/RegistrarDatos.js
@@ -400,7 +400,7 @@ const RegistrarDatos = ({ setNForm, data, setData }) => {
               {!passwordStatus.pass && passwordStatus.message && (
                 <p className="text-danger mw-100">{passwordStatus.message}</p>
               )}
-              <span className="password-icon " onClick={toggleShowPassword}>
+              <span className="password-icon" style={{ cursor: "pointer" }} onClick={toggleShowPassword}>
                 {showPassword ? <FaEyeSlash /> : <FaEye />}
               </span>
             </div>
@@ -435,7 +435,7 @@ const RegistrarDatos = ({ setNForm, data, setData }) => {
               <p className="text-danger input-width-30">
                 {errors.confirmar_contraseña}
               </p>
-              <span className="password-icon" onClick={toggleShowPassword1}>
+              <span className="password-icon" style={{ cursor: "pointer" }} onClick={toggleShowPassword}>
                 {showPassword1 ? <FaEyeSlash /> : <FaEye />}
               </span>
             </div>
