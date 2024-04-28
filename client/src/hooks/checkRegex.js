2fb136bf6e03d9bdb980171e06fdbf46298a9b26
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2024-04-28 12:40:48 -0700

    corregir mensajes y errores ortograficos

--- client/src/hooks/checkRegex.js
+++ client/src/hooks/checkRegex.js
@@ -3,7 +3,7 @@
 const checkPassword = () => {
     return (password) =>{
         if (password.length < 8) {
-            return {pass : false,message : "La contraseña debe ser mayor a 8 caracteres"}
+            return {pass : false,message : "La contraseña debe tener al menos 8 caracteres"}
         }
         if (!password.match(/[a-z]/g)){
             return {pass : false, message : "La contraseña debe tener al menos una letra en minúscula"}
@@ -12,10 +12,10 @@ const checkPassword = () => {
             return {pass : false, message : "La contraseña debe tener al menos una letra en mayúscula"}
         }
         if (!password.match(/\d/g)){
-            return {pass : false, message : "La contraña debe tener al menos un numero"}
+            return {pass : false, message : "La contraña debe tener al menos un número"}
         }
         if (!password.match(/[#!@$%&-*+/?¡]+/g)){
-            return {pass : false, message : "La contraseña debe tener al menos un carácter especial."}
+            return {pass : false, message : "La contraseña debe tener al menos un caracter especial"}
         }
         return {pass : true}
     }
