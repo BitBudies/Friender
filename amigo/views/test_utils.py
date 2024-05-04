611f4fcb230eee07099a21a96e73d8f286ff4617
Frosmin <simonabastomart@gmail.com>
2024-05-04 11:08:12 -0400

    tests por ceparado

--- amigo/views/test_utils.py
+++ amigo/views/test_utils.py
@@ -1,13 +1,30 @@
 from django.test import TestCase
 from .utils import correo_valido, generate_key
 
+import unittest 
+
+
 class TestUtils(TestCase):
     def test_correo_valido(self):
         self.assertTrue(correo_valido("ricardorijas@gmail.com"))
+        
+    pass
+class TestUtils(unittest.TestCase):
+    def test_correo_valido(self):
         self.assertTrue(correo_valido("simonabastomart@gmail.com"))
+    pass
+
+class TestUtils(unittest.TestCase):
+    def test_correo_valido(self):
         self.assertTrue(correo_valido("12456545@est.umss.edu"))
+    pass
+
+class TestUtils(unittest.TestCase):
+    def test_correo_valido(self):
         self.assertTrue(correo_valido("juanto.de.dehua@gmail.com"))
+    pass
+
+class TestUtils(unittest.TestCase):
+    def test_correo_valido(self):
         self.assertTrue(correo_valido("pepejuaa@hotmail.com"))
     pass
-        
-        
