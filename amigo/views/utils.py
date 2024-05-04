1c665c73e71acb6f4bb6198aeb1a4b6b1fb9d5aa
Frosmin <simonabastomart@gmail.com>
2024-05-04 11:27:03 -0400

    validar contraseña

--- amigo/views/utils.py
+++ amigo/views/utils.py
@@ -12,12 +12,12 @@ def obtener_csrf(request):
 
 
 def correo_valido(correo):
-    # (✿◡‿◡)
     regex_correo = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     if re.match(regex_correo, correo):
         return True
     else:
         return False
+    
 
 def generate_key(length):
     characters = string.ascii_letters + string.digits + '-_'
@@ -25,3 +25,10 @@ def generate_key(length):
 
 if __name__ == "__main__":
     print(len(generate_key(64)))
+    
+def contrasena_valida(contrasena):
+    regex_contrasena = r'^(?!-)(?!\.)(?!@)[a-zA-Z0-9!()\-.?[\]_`~;:#$%&*+=^{}]+$'
+    if re.match(regex_contrasena, contrasena):
+        return True
+    else:
+        return False
