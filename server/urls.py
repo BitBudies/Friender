a4c24bfb2a06ca023ee1b1086804a7008d88dc2a
Jh0nZ <jhondeycraft776@gmail.com>
2024-05-06 19:13:57 -0400

    uwu

--- server/urls.py
+++ server/urls.py
@@ -101,6 +101,6 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), 
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
-    
+    # primer commit del 3er sprint
   #  path('correo/', EnviarCorreo.as_view())
 ]
