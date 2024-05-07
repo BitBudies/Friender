dfb30ec72c0988714b4ee17bc47c69db17ab61e5
Daye0210 <202202138@est.umss.edu>
2024-05-07 17:30:41 -0400

    calificacion

--- amigo/views/lista_solicitudes_aceptadas.py
+++ amigo/views/lista_solicitudes_aceptadas.py
@@ -4,3 +4,6 @@ from ..models import Calificacion
 def  ObtenerListaDeSolicitudes(request, amigo_id):
     solicitudes_aceptadas = solicitud_alquiler.objects.filter(amigo_id=amigo_id, estado_solicitud='A')
     data = []
+    for solicitud in solicitudes_aceptadas:
+        #calificación del cliente
+        calificacion_cliente = Calificacion.objects.filter(cliente=solicitud.cliente).first()
