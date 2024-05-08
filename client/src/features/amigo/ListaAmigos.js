1ab24977bfa2c2fcaeef0239b11b647866c418cb
Dayana200505 <128557082+Dayana200505@users.noreply.github.com>
2024-05-08 16:08:50 -0700

    barra de buscar

--- client/src/features/amigo/ListaAmigos.js
+++ client/src/features/amigo/ListaAmigos.js
@@ -10,6 +10,7 @@ import { MdInterests, MdOutlineAttachMoney } from "react-icons/md";
 import { BsCalendarRange } from "react-icons/bs";
 import { IoLocationSharp, IoClose } from "react-icons/io5";
 import { IoIosPeople } from "react-icons/io";
+import { FaFilter } from "react-icons/fa";
 
 const calificacionEstrellas = (calificacion) => {
   const numEstrellas = Math.round(calificacion);
@@ -22,7 +23,14 @@ const ListaAmigos = () => {
   const token = cookies.token;
 
   const { n_page } = useParams();
-  const [values, setValues] = useState({ genero: "" }); // Define defaultValues aquí
+  const [values, setValues] = useState({
+    intereses: [],
+    rangoEdad: "",
+    precio: { min: "", max: "" },
+    genero: "",
+    ubicacion: "",
+  });
+
   const { pageRef, goToBeginning } = useGlobalContext();
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -40,169 +48,241 @@ const ListaAmigos = () => {
     pagina: n_page,
     limite: 24,
     token,
+    // Aquí deberías pasar los valores de los filtros
+    // valores: values
   });
-  const [interesesSeleccionados, SetInteresesSeleccionados] = useState([
+
+  const [interesesSeleccionados, setInteresesSeleccionados] = useState([
     "aa",
     "bb",
     "cc",
   ]);
+
+
+  const interesesPermitidos = [
+    "Arte y Cultura",
+    "Cine y Series",
+    "Gastronomía",
+    "Idiomas",
+    "Lectura",
+    "Taekwondo",
+    "Tecnología",
+  ];
+
+  const ubicacionesPermitidas = [
+    "Beni",
+    "Chuquisaca",
+    "Cochabamba",
+    "La Paz",
+    "Oruro",
+    "Pando",
+    "Potosí",
+    "Santa Cruz",
+    "Tarija",
+  ];
+
+  useEffect(() => {
+    // Ordenar intereses seleccionados alfabéticamente
+    setInteresesSeleccionados(
+      interesesSeleccionados.sort((a, b) => a.localeCompare(b))
+    );
+  }, [interesesSeleccionados]);
 
   if (isFetching) {
     return <Loading />;
   } else if (isSuccess) {
     return (
       <div id="lista_amigos" className="page bg-light" ref={pageRef}>
-        <div className="filtrosYBoton">
+        <div className="filtrosYBoton   d-flex justify-content-center">
           <div className="rectangle">
             <div>
-              <label htmlFor="genero" className="input-label ">
+              <label htmlFor="intereses" className="input-label">
                 <MdInterests />
                 Intereses
               </label>
               <select
-                id="genero"
-                name="genero"
-                value={values.genero}
+                id="intereses"
+                name="intereses"
+                value={values.intereses}
                 onChange={handleChange}
                 className="form-select"
+                
               >
-                <option value="" disabled selected hidden>
-                  ----------
-                </option>
-                <option value="masculino">Masculino</option>
-                <option value="femenino">Femenino</option>
-                <option value="otro">Otro</option>
+                {interesesPermitidos.map((interes) => (
+                  <option key={interes} value={interes}>
+                    {interes}
+                  </option>
+                ))}
               </select>
             </div>
 
             <div>
-              <label htmlFor="genero" className="input-label ">
+              <label htmlFor="rangoEdad" className="input-label">
                 <BsCalendarRange /> Rango de edad
               </label>
               <select
-                id="genero"
-                name="genero"
-                value={values.genero}
+                id="rangoEdad"
+                name="rangoEdad"
+                value={values.rangoEdad}
                 onChange={handleChange}
                 className="form-select"
               >
-                <option value="" disabled selected hidden>
-                  ----------
-                </option>
-                <option value="1">18 a 25</option>
-                <option value="2">26 a 35</option>
-                <option value="3">36 a 45</option>
+                <option value="">----------</option>
+                <option value="1">Entre 18 y 25 años</option>
+                <option value="2">Entre 25 y 35 años</option>
+                <option value="3">Entre 35 y 45 años</option>
+                <option value="4">Entre 45 y 55 años</option>
+                <option value="5">Entre 55 y 65 años</option>
+                <option value="6">Más de 65 años</option>
               </select>
             </div>
 
             <div>
-              <label htmlFor="genero" className="input-label ">
+              <label htmlFor="precio" className="input-label">
                 <MdOutlineAttachMoney />
                 Precio
               </label>
               <div className="precios">
- 
-                <input type="text" placeholder="Min"/>
+                <input
+                  type="text"
+                  placeholder="Min"
+                  value={values.precio.min}
+                  onChange={(e) =>
+                    setValues({ ...values, precio: { ...values.precio, min: e.target.value } })
+                  }
+                />
                 <span> a </span>
-                <input type="text" placeholder="Max"/>
+                <input
+                  type="text"
+                  placeholder="Max"
+                  value={values.precio.max}
+                  onChange={(e) =>
+                    setValues({ ...values, precio: { ...values.precio, max: e.target.value } })
+                  }
+                />
               </div>
             </div>
 
             <div>
-              <label htmlFor="genero" className="input-label ">
-                <IoIosPeople /> Género
-              </label>
-              <select
-                id="genero"
-                name="genero"
-                value={values.genero}
-                onChange={handleChange}
-                className="form-select"
-              >
-                <option value="" disabled selected hidden>
-                  ----------
-                </option>
-                <option value="masculino">Masculino</option>
-                <option value="femenino">Femenino</option>
-                <option value="otro">Otro</option>
-              </select>
-            </div>
+  <label className="input-label">
+    <IoIosPeople /> Género
+  </label>
+  <div className="form-check">
+    <input
+      type="checkbox"
+      id="masculino"
+      name="genero"
+      value="masculino"
+      checked={values.genero === "masculino"}
+      onChange={handleChange}
+      className="form-check-input"
+    />
+    <label htmlFor="masculino" className="form-check-label">Masculino</label>
+  </div>
+  <div className="form-check">
+    <input
+      type="checkbox"
+      id="femenino"
+      name="genero"
+      value="femenino"
+      checked={values.genero === "femenino"}
+      onChange={handleChange}
+      className="form-check-input"
+    />
+    <label htmlFor="femenino" className="form-check-label">Femenino</label>
+  </div>
+  <div className="form-check">
+    <input
+      type="checkbox"
+      id="otro"
+      name="genero"
+      value="otro"
+      checked={values.genero === "otro"}
+      onChange={handleChange}
+      className="form-check-input"
+    />
+    <label htmlFor="otro" className="form-check-label">Otro</label>
+  </div>
+</div>
 
             <div>
-              <label htmlFor="genero" className="input-label ">
+              <label htmlFor="ubicacion" className="input-label">
                 <IoLocationSharp /> Ubicación
               </label>
               <select
-                id="genero"
-                name="genero"
-                value={values.genero}
+                id="ubicacion"
+                name="ubicacion"
+                value={values.ubicacion}
                 onChange={handleChange}
                 className="form-select"
               >
-                <option value="" disabled selected hidden>
-                  ----------
-                </option>
-                <option value="c">Cochabamba</option>
-                <option value="l">La Paz</option>
-                <option value="s">Santa Cruz</option>
-                <option value="">...</option>
+                <option value="">----------</option>
+                {ubicacionesPermitidas.map((ubicacion) => (
+                  <option key={ubicacion} value={ubicacion}>
+                    {ubicacion}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
-          <button>Filtrar</button>
+          <div className="btn-container">
+            <button className="btn btn-azul mt-3 btn-solicitar">
+              <FaFilter /> Filtrar
+            </button>
+          </div>
         </div>
         <div className="interesesSeleccionados">
-          {interesesSeleccionados.map((interes) => {
-            return <div className="burbujaInteres">{interes} <IoClose className="cerrarBurbuja" onClick={() => console.log(`borrar el interes ${interes}`)}/></div>;
-          })}
+          {interesesSeleccionados.map((interes) => (
+            <div key={interes} className="burbujaInteres">
+              {interes}{" "}
+              <IoClose
+                className="cerrarBurbuja"
+                onClick={() => setInteresesSeleccionados(interesesSeleccionados.filter((i) => i !== interes))}
+              />
+            </div>
+          ))}
         </div>
 
         <div className="container-fluid py-5">
           <div className="row row-cols-1 row-cols-lg-4 row-cols-md-3 g-3">
-            {amigos["amigos"].map((amigo, index) => {
-              return (
-                <div key={index} className="col">
-                  <div className="card-amigo card card-list">
-                    <div
-                      className="card-header"
-                      style={{
-                        backgroundImage: `url(${
-                          amigo.imagenBase64
-                            ? "data:image/jpeg;base64," + amigo.imagenBase64
-                            : "/images/user.jpeg"
-                        })`,
-                      }}
-                    />
-                    {/* <h5 className='card-title'>{amigo.cliente_id}</h5> */}
-                    <div className="card-body px-4">
-                      <h5 className="card-title">{amigo.nombre_completo}</h5>
-                      <div className="card-text">
-                        <div className="card-stats">
-                          <div className="text-warning">
-                            {calificacionEstrellas(amigo.calificacion)}
-                          </div>
-                          <div className="card-n-users">
-                            0{" "}
-                            <span>
-                              <FaUser />
-                            </span>
-                          </div>
+            {amigos["amigos"].map((amigo, index) => (
+              <div key={index} className="col">
+                <div className="card-amigo card card-list">
+                  <div
+                    className="card-header"
+                    style={{
+                      backgroundImage: `url(${
+                        amigo.imagenBase64
+                          ? "data:image/jpeg;base64," + amigo.imagenBase64
+                          : "/images/user.jpeg"
+                      })`,
+                    }}
+                  />
+                  <div className="card-body px-4">
+                    <h5 className="card-title">{amigo.nombre_completo}</h5>
+                    <div className="card-text">
+                      <div className="card-stats">
+                        <div className="text-warning">
+                          {calificacionEstrellas(amigo.calificacion)}
                         </div>
-                        <div className="card-actions">
-                          <Link
-                            to={`/amigos/${amigo.amigo_id}`}
-                            className="btn btn-azul "
-                          >
-                            Ver Perfil
-                          </Link>
-                          {amigo.precio_amigo} Bs/hr
+                        <div className="card-n-users">
+                          0{" "}
+                          <span>
+                            <FaUser />
+                          </span>
                         </div>
+                      </div>
+                      <div className="card-actions">
+                        <Link to={`/amigos/${amigo.amigo_id}`} className="btn btn-azul ">
+                          Ver Perfil
+                        </Link>
+                        {amigo.precio_amigo} Bs/hr
                       </div>
                     </div>
                   </div>
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
         </div>
 
@@ -222,26 +302,24 @@ const ListaAmigos = () => {
                 {"<"}{" "}
               </Link>
             </li>
-            {Array.from({ length: amigos.numero_paginas }, (_, index) => {
-              return (
-                <li
-                  key={index}
-                  className={`pagination-item page-item ${
-                    Number(n_page) === index + 1 && "active"
+            {Array.from({ length: amigos.numero_paginas }, (_, index) => (
+              <li
+                key={index}
+                className={`pagination-item page-item ${
+                  Number(n_page) === index + 1 && "active"
+                }`}
+              >
+                <Link
+                  className={`page-link ${
+                    Number(n_page) === index + 1 && "bg-azul-fuerte"
                   }`}
+                  to={`/amigos/page/${index + 1}`}
+                  onClick={goToBeginning}
                 >
-                  <Link
-                    className={`page-link ${
-                      Number(n_page) === index + 1 && "bg-azul-fuerte"
-                    }`}
-                    to={`/amigos/page/${index + 1}`}
-                    onClick={goToBeginning}
-                  >
-                    {index + 1}
-                  </Link>
-                </li>
-              );
-            })}
+                  {index + 1}
+                </Link>
+              </li>
+            ))}
             <li className="page-item">
               <Link
                 className={`page-link ${
