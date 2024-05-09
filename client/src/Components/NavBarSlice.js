143042cab7a6691104d03b435763f64ee2cb4d61
Richi78 <ricardo.rojas.carvajal@gmail.com>
2024-05-09 16:08:40 -0300

    mi perfil y estado si es amiwo

---
+++ client/src/Components/NavBarSlice.js
@@ -0,0 +1,25 @@
+import { apiSlice } from "../features/api/apiSlice";
+
+const navBarApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getEsAmigo: builder.query({
+        query: (token) => ({
+          url: `/clienteesamigo`,
+          headers: {
+            Authorization: `Token ${token}`
+          },
+
+        }),
+    }),
+    getClienteById: builder.query({
+        query: ({ id_cliente, token }) => ({
+          url: `/cliente/${id_cliente}`,
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }),
+    }),
+  }),
+});
+
+export const { useGetEsAmigoQuery, useGetClienteByIdQuery } = navBarApi;
