a64410ca0bf36f76a18ab63424b73483b1d1cd09
Daye0210 <202202138@est.umss.edu>
2024-05-10 14:57:29 -0400

    incluir todas las imagenes

--- amigo/views/lista_solicitudes_aceptadas.py
+++ amigo/views/lista_solicitudes_aceptadas.py
@@ -1,16 +1,18 @@
-from django.http import JsonResponse
-from ..models import Calificacion
-from ..models import solicitud_alquiler, Cliente, Amigo
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from rest_framework import status
 from django.shortcuts import get_object_or_404
 from datetime import datetime
-from django.db.models import F
+from django.db.models import F, ExpressionWrapper, DateTimeField
 import base64
+from ..models import Calificacion, solicitud_alquiler, Amigo
 from amigo.models.fotografiaDB import Fotografia
-from django.db.models import ExpressionWrapper, DateTimeField
-
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
@@ -27,33 +29,30 @@ def ObtenerListaDeSolicitudes(request):
         )
     ).order_by('fecha_hora_inicio')
 
-    data = []
+    data = {"solicitudes_recibidas": []}
     for solicitud in solicitudes_ordenadas:
         if solicitud.fecha_inicio > now.date() or (solicitud.fecha_inicio == now.date() and solicitud.hora_inicio > now.time()):
             calificacion_cliente = Calificacion.objects.filter(cliente=solicitud.cliente).first()
-            primera_imagen = obtener_primera_imagen_cliente(amigo.cliente)
-            solicitud_info = {
-                'nombre_cliente': solicitud.cliente.nombre,
+            imagenes_cliente = obtener_imagenes_cliente(solicitud.cliente)
+            solicitud_data = {
+                'nombre_cliente': solicitud.cliente.getFullName(),
                 'calificacion_cliente': calificacion_cliente.puntuacion if calificacion_cliente else 0,
                 'lugar': solicitud.lugar,
                 'fecha_inicio': solicitud.fecha_inicio,
                 'duracion': solicitud.minutos,
+                'precio': solicitud.precio,
                 'hora_inicio': solicitud.hora_inicio,
-                'imagenBase64': primera_imagen["imagenBase64"] if primera_imagen else None
+                'imagenes': imagenes_cliente,
             }
-            data.append(solicitud_info)
+            data["solicitudes_recibidas"].append(solicitud_data)
 
-    return JsonResponse(data, safe=False)
+    return Response(data)
 
-def obtener_primera_imagen_cliente(cliente):
-    fotografia_primera = Fotografia.objects.filter(cliente=cliente, estado_fotografia='F').order_by('prioridad').first()
-    if fotografia_primera:
-        imagenBase64 = None
-        if fotografia_primera.imagenBase64:
-            imagenBase64 = base64.b64encode(fotografia_primera.imagenBase64).decode("utf-8")
-        return {
-            "imagenBase64": imagenBase64,
-            "prioridad": fotografia_primera.prioridad
-        }
-    return None
+def obtener_imagenes_cliente(cliente):
+    imagenes = []
+    fotografia_amigo = Fotografia.objects.filter(cliente=cliente, prioridad=0).first()
+    if fotografia_amigo:
+        imagen_base64 = base64.b64encode(fotografia_amigo.imagenBase64).decode("utf-8")
+        imagenes.append({"imagenBase64": imagen_base64})
+    return imagenes
 
