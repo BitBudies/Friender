4539f834beb546cc8769baafbae7b06fcff9e3e8
Kevin Huayllas <KevinOmega01@gmail.com>
2024-05-09 21:58:40 -0400

    cambiar nombre de url perfil a cuenta de amigo

--- client/src/App.js
+++ client/src/App.js
@@ -67,7 +67,7 @@ function App() {
           <Route path='/cliente/:id_cliente' element={<PerfilCliente/>}/>
           <Route path='/new-password/:tokencito' element={<NewPassword/>}/>
           <Route path='/login' element= {<LogIn/>}/>
-          <Route path='/perfil' element={<Perfil/>}/>
+          <Route path='/cuenta-amigo' element={<Perfil/>}/>
           <Route path='/usuario/solicitud_pendiente/:id_solicitud' element={<SolicitudDetalles/>}/>
           <Route path="/test/jhon" element={<Jhon/>} />
           <Route path="/resetPassword" element={<ResetPassword/>}/>
