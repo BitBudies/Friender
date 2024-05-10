f895eabd0b7e18633a1c41edcb9a5289273ea661
Daye0210 <202202138@est.umss.edu>
2024-05-09 21:28:28 -0400

    requerido

--- amigo/views/lista_solicitudes_aceptadas.py
+++ amigo/views/lista_solicitudes_aceptadas.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
-from ..models import solicitud_alquiler
 from ..models import Calificacion
+from ..models import solicitud_alquiler, Cliente, Amigo
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 def  ObtenerListaDeSolicitudes(request, amigo_id):
     solicitudes_aceptadas = solicitud_alquiler.objects.filter(amigo_id=amigo_id, estado_solicitud='A')
     data = []
