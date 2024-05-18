ea707fcd266410e29a3b6061b6414dac89be4a16
Daye0210 <202202138@est.umss.edu>
2024-05-18 16:18:53 -0400

    cambiando cursor

--- client/src/Components/CartSolicitudesAccept/SolicitudAceptada.jsx
+++ client/src/Components/CartSolicitudesAccept/SolicitudAceptada.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
+import React from 'react';
 import "./SolicitudAceptada.css";
 import { FaClock } from "react-icons/fa6";
 import { FaCalendarAlt, FaMapMarkerAlt } from "react-icons/fa";
 import { MdUpdate } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
 
-const SolicitudAceptada = ( {imagenBase64, 
-                            nombre_cliente, 
-                            fecha_inicio, 
-                            hora_inicio, 
-                            lugar, 
-                            solicitud_aceptada_id,
-                            duracion,
-                            dias_restantes
-                          } ) => { 
- 
+const SolicitudAceptada = ({
+  imagenBase64,
+  nombre_cliente,
+  fecha_inicio,
+  hora_inicio,
+  lugar,
+  solicitud_aceptada_id,
+  duracion,
+  dias_restantes
+}) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/solicitud-detalles/${solicitud_aceptada_id}`);
+  };
+
   function formatFecha(fecha) {
     const [year, month, day] = fecha.split("-");
     return `${day.padStart(2, "0")}/${month.padStart(2, "0")}/${year}`;
   }
-                              
+
   return (
-    <div className="col-container ">
-      <div className="solicitud-aceptada-card ">
+    <div className="col-container" key={solicitud_aceptada_id} onClick={handleClick}>
+      <div className="solicitud-aceptada-card">
         <div id="datos-perfil">
-          <div id="datos-perfil">
-            <div
-              className="card-image mb-2"
-              style={{
-                backgroundImage: `url(${
-                  imagenBase64
-                    ? `data:image/jpeg;base64,${imagenBase64}`
-                    : "/images/user.jpeg"
-                })`,
-              }}
-            ></div>
+          <div className="card-image mb-2"
+               style={{
+                 backgroundImage: `url(${imagenBase64 ? `data:image/jpeg;base64,${imagenBase64}` : "/images/user.jpeg"})`,
+               }}>
           </div>
           <h5>{nombre_cliente}</h5>
         </div>
@@ -53,7 +53,6 @@ const SolicitudAceptada = ( {imagenBase64,
               <p>{hora_inicio.match(/\d+:\d+/)}</p>
             </div>
           </div>
-
           <div className="card-item-aceptadas">
             <div>
               <span>
@@ -64,22 +63,21 @@ const SolicitudAceptada = ( {imagenBase64,
             <p>{duracion} Hrs</p>
           </div>
           <div className="align-items-center">
-            {
-              dias_restantes===0 ?
-                <div className='hoy-jaja'>
-                  <MdUpdate size={25}/>
-                  <h5><strong>Hoy</strong></h5>
-                </div>
-              :
-                dias_restantes===1 ?
-                <p className='mañana-uwu'>En 1 día</p>
-                : <p className='mañana-uwu'>En {dias_restantes} días</p>   
-            }       
+            {dias_restantes === 0 ? (
+              <div className='hoy-jaja'>
+                <MdUpdate size={25}/>
+                <h5><strong>Hoy</strong></h5>
+              </div>
+            ) : dias_restantes === 1 ? (
+              <p className='mañana-uwu'>En 1 día</p>
+            ) : (
+              <p className='mañana-uwu'>En {dias_restantes} días</p>
+            )}       
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default SolicitudAceptada
+export default SolicitudAceptada;
