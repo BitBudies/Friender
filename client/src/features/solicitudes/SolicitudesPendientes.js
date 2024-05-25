942e877a6420c5389070a2072d06c5cc7ad8a950
Jh0nZ <jhondeycraft776@gmail.com>
2024-05-25 17:16:02 -0400

    agregar mensaje cuando no hay solicitudes pendientes

--- client/src/features/solicitudes/SolicitudesPendientes.js
+++ client/src/features/solicitudes/SolicitudesPendientes.js
@@ -24,9 +24,13 @@ const SolicitudesPendientes = () => {
           <h1 id="titulo-solicitudes">Solicitudes Recibidas</h1>
         </div>
         <div className="solicitudes-pendientes-center ">
-          {data.solicitudes_recibidas.map((item, index) => (
+          {data.solicitudes_recibidas.lenght > 0 ? (
+          data.solicitudes_recibidas.map((item, index) => (
             <Solicitud key={index} solicitud={item} index={index} />
-          ))}
+          ))) : (
+            <p>No hay solicitudes pendientes</p>
+          )
+        }
         </div>
       </div>
     );
